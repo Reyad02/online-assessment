0b63b88683a926a3d0048a48f0a0a91a2eda799d
unknown <areyad203011@bscse.uiu.ac.bd>
2025-04-25 16:32:51 +0600

    feature part 2 done

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// const features = document.querySelectorAll(".feature-2-details");
+// features.forEach((feature) => {
+//   feature.addEventListener("hover", () => {
+//     feature.style.borderLeftColor = "red";
+//   });
+
+//   feature.addEventListener("mouseleave", () => {
+//     feature.style.borderLeftColor = "#E0E7FF";
+//   });
+// });
